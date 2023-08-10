02073c1c38ebed81caef0b9d4e5b6855326c4f9d
Shih-Ling Shen <114762019+shihlings@users.noreply.github.com>
2023-08-10 15:44:04 -0700

    Motor Testing

---
+++ thunderbird_internal/motor_ramp_test/motor_ramp_test.ino
@@ -0,0 +1,54 @@
+/*
+ * Date Created:    August 10th, 2023
+ * Last Modified:   August 10th, 2023
+ * Filename:        motor_ramp_test.ino
+ * Purpose:         Ramp up then down (both clockwise and counterclockwise) the DC Motor (FIT0521) using PWM.
+ * Microcontroller: Arduino Uno R3
+ * Connections:     See controller_schematics located in the schematics folder
+ */
+
+// define constants and pins 
+  #define PWM1          5
+  #define PWM2          6
+  #define RAMP_INTERVAL 100
+  #define RAMP_PAUSE    1000
+
+// setup
+void setup() {
+  // setup pins for motor pwm
+  pinMode(PWM1, OUTPUT);
+  pinMode(PWM2, OUTPUT);
+}
+
+// main loop
+void loop() {
+  // ramp up counterclockwise
+  digitalWrite(PWM2, 0);
+  for (int index = 0; index <= 255; index++) {
+    analogWrite(PWM1, index);
+    delay(RAMP_INTERVAL);
+  }
+  delay(RAMP_PAUSE);
+
+  // ramp down counterclockwise
+  for (int index = 255; index >= 0; index--) {
+    analogWrite(PWM1, index);
+    delay(RAMP_INTERVAL);
+  }
+  delay(RAMP_PAUSE);
+
+  // ramp up clockwise
+  digitalWrite(PWM1, 0);
+  for (int index = 0; index <= 255; index++) {
+    analogWrite(PWM2, index);
+    delay(RAMP_INTERVAL);
+  }
+  delay(RAMP_PAUSE);
+
+  // ramp down clockwise
+  for (int index = 255; index >= 0; index--) {
+    analogWrite(PWM2, index);
+    delay(RAMP_INTERVAL);
+  }
+  delay(RAMP_PAUSE);
+}
