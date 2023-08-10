0db62ce260f24fd150b1acff7e5b86297b03a0d0
Shih-Ling Shen <114762019+shihlings@users.noreply.github.com>
2023-08-10 15:44:22 -0700

    Adjusted to one joystick

--- controller/controller_firmware/controller_firmware.ino
+++ controller/controller_firmware/controller_firmware.ino
@@ -8,34 +8,19 @@
  * Notes:           Disconnect the RF module if there are issues uploading the sketch.
  */
 
-//#define DEBUG // uncomment this line when trying to debug the code with a computer
+#define DEBUG // uncomment this line when trying to debug the code with a computer
 
 // Pin definitions
   #define RF_SERIAL_RATE     9600    //change this to the baud rate that the RF communication module is set at
   #define DEBUG_SERIAL_RATE  115200
-  #define RUDR_BUTTON        2
-  #define RUDR_VERT          A1
-  #define RUDR_HORZ          A2
-  #define SAIL_BUTTON        3
-  #define SAIL_VERT          A3
-  #define SAIL_HORZ          A4
-
-// Function declarations
-  void getJoystick(int8_t* rudr_stat, int8_t* sail_stat);
-  void sendSeralRF(int8_t rudr_stat, int8_t sail_stat);
-  void sendSerialDebug(uint16_t rudr_val, uint16_t sail_val);
+  #define RUDR               A4
+  #define SAIL               A3
 
 // Setup
 void setup() {
-  // Pin Setup for RUDDER joystick (BUTTON and Horizontal Axis disabled for this application)
-  // pinMode(RUDR_BUTTON, INPUT);
-  pinMode(RUDR_VERT, INPUT);
-  // pinMode(RUDR_HORZ, INPUT);
-
-  // Pin Setup for SAIL joystick (BUTTON and Horizontal Axis disabled for this application)
-  // pinMode(SAIL_BUTTON, INPUT);
-  pinMode(SAIL_VERT, INPUT);
-  // pinMode(SAIL_HORZ, INPUT);
+  // Pin Setup for joystick
+  pinMode(RUDR, INPUT);
+  pinMode(SAIL, INPUT);
   
   #ifdef DEBUG
     //Begin serial communication to computer
@@ -66,8 +51,8 @@ void loop() {
 // get joystick status
 void getJoystick(uint16_t* rudr_val, uint16_t* sail_val) {
   // initialize raw value variables and obtain raw values from analog ports
-  *rudr_val = analogRead(RUDR_VERT);
-  *sail_val = analogRead(SAIL_VERT);
+  *rudr_val = analogRead(RUDR);
+  *sail_val = analogRead(SAIL);
 }
 
 // send serial message to RF module to instruct what to send
