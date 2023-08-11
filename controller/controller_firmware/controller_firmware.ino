c710b573eed357fa7bddbeada5a524eea07c2b0d
Shih-Ling Shen <114762019+shihlings@users.noreply.github.com>
2023-08-11 16:51:07 -0700

    added RF communication functionality

--- controller/controller_firmware/controller_firmware.ino
+++ controller/controller_firmware/controller_firmware.ino
@@ -1,6 +1,6 @@
 /*
  * Date Created:    August 5th, 2023
- * Last Modified:   August 7th, 2023
+ * Last Modified:   August 11th, 2023
  * Filename:        controller_firmware.ino
  * Purpose:         Process input data from two joystics and send them using an RF communication module to Thunderbird.
  * Microcontroller: Arduino Uno R3
@@ -8,13 +8,16 @@
  * Notes:           Disconnect the RF module if there are issues uploading the sketch.
  */
 
-#define DEBUG // uncomment this line when trying to debug the code with a computer
+//#define DEBUG // uncomment this line when trying to debug the code with a computer
 
 // Pin definitions
   #define RF_SERIAL_RATE     9600    //change this to the baud rate that the RF communication module is set at
-  #define DEBUG_SERIAL_RATE  115200
+  #define DEBUG_SERIAL_RATE  9600
   #define RUDR               A4
   #define SAIL               A3
+
+// RF definition
+  #define SENDING_INTERVAL   750
 
 // Setup
 void setup() {
@@ -31,6 +34,9 @@ void setup() {
   #endif
 }
 
+//store last rf message sent time
+uint32_t last_send_time;
+
 void loop() {
   // initialize joystic status variables
   uint16_t rudr_val = 0;
@@ -43,8 +49,11 @@ void loop() {
     // if debugging mode is on, send raw values
     sendSerialDebug(rudr_val, sail_val);
   #else
-    // Send message via RF if debugging is off
-    sendSeralRF(rudr_val, sail_val);
+    if(millis() >= last_send_time + SENDING_INTERVAL){
+      // Send message via RF if debugging is off and sending interval has expired
+      sendSeralRF(rudr_val, sail_val);
+      last_send_time = millis();
+    }
   #endif
 }
 
@@ -58,6 +67,7 @@ void getJoystick(uint16_t* rudr_val, uint16_t* sail_val) {
 // send serial message to RF module to instruct what to send
 void sendSeralRF(uint16_t rudr_val, uint16_t sail_val) {
   Serial.print(rudr_val);
+  Serial.print(";");
   Serial.println(sail_val);
 }
 
