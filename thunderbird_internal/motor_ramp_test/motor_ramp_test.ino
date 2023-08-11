7d4d1a959c7b018ed83be8387b4f9c2e069764fa
Shih-Ling Shen <114762019+shihlings@users.noreply.github.com>
2023-08-11 16:55:42 -0700

    added encoder reading

--- thunderbird_internal/motor_ramp_test/motor_ramp_test.ino
+++ thunderbird_internal/motor_ramp_test/motor_ramp_test.ino
@@ -1,11 +1,14 @@
 /*
  * Date Created:    August 10th, 2023
- * Last Modified:   August 10th, 2023
+ * Last Modified:   August 11th, 2023
  * Filename:        motor_ramp_test.ino
  * Purpose:         Ramp up then down (both clockwise and counterclockwise) the DC Motor (FIT0521) using PWM.
  * Microcontroller: Arduino Uno R3
  * Connections:     See internal_schematics located in the schematics folder
  */
+
+#include <Encoder.h>
+Encoder myEnc(2,3);
 
 // define constants and pins 
   #define PWM1          5
@@ -13,9 +16,12 @@
   #define RAMP_INTERVAL 100
   #define RAMP_PAUSE    1000
 
+long prev_pos  = -999;
+
 // setup
 void setup() {
   // setup pins for motor pwm
+  Serial.begin(9600);
   pinMode(PWM1, OUTPUT);
   pinMode(PWM2, OUTPUT);
 }
@@ -26,13 +32,14 @@ void loop() {
   digitalWrite(PWM2, 0);
   for (int index = 0; index <= 255; index++) {
     analogWrite(PWM1, index);
+    readEncoder();
     delay(RAMP_INTERVAL);
   }
-  delay(RAMP_PAUSE);
 
   // ramp down counterclockwise
   for (int index = 255; index >= 0; index--) {
     analogWrite(PWM1, index);
+    readEncoder();
     delay(RAMP_INTERVAL);
   }
   delay(RAMP_PAUSE);
@@ -41,14 +48,26 @@ void loop() {
   digitalWrite(PWM1, 0);
   for (int index = 0; index <= 255; index++) {
     analogWrite(PWM2, index);
+    readEncoder();
     delay(RAMP_INTERVAL);
   }
-  delay(RAMP_PAUSE);
 
   // ramp down clockwise
   for (int index = 255; index >= 0; index--) {
     analogWrite(PWM2, index);
+    readEncoder();
     delay(RAMP_INTERVAL);
   }
   delay(RAMP_PAUSE);
+}
+
+// read current encoder value, if there is a change, store the change and print on serial port
+void readEncoder () {
+  long pos = myEnc.read();
+
+  // store and print on serial port if change is detected
+  if (pos != prev_pos) {
+    prev_pos = pos;
+    Serial.println(pos);
+  }
 }
