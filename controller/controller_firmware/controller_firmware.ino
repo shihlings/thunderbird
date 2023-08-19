04d56120603adb0917117924605c457ca4f0022d
Shih-Ling Shen <114762019+shihlings@users.noreply.github.com>
2023-08-19 11:59:54 -0700

    added averaging signals to prevent sudden jumps and extreme values

--- controller/controller_firmware/controller_firmware.ino
+++ controller/controller_firmware/controller_firmware.ino
@@ -1,6 +1,6 @@
 /*
  * Date Created:    August 5th, 2023
- * Last Modified:   August 11th, 2023
+ * Last Modified:   August 19th, 2023
  * Filename:        controller_firmware.ino
  * Purpose:         Process input data from two joystics and send them using an RF communication module to Thunderbird.
  * Microcontroller: Arduino Uno R3
@@ -23,6 +23,13 @@
   #define SENDING_INTERVAL   150
   SoftwareSerial rfSerial(11, 10);        // RX, TX Pins
 
+// Variables 
+  #define  MEAN_NUM             5         // Defines the number of past values to average
+  uint32_t last_send_time;                // Store last RF send time
+  uint16_t rudr_val[MEAN_NUM];            // Store an array of past rudder values
+  uint16_t sail_val[MEAN_NUM];            // Store an array of past sail values
+  uint8_t  val_index = 0;                 // Current position of the index for the two arrays
+
 // Setup
 void setup() {
   // Pin Setup for joystick
@@ -30,47 +37,65 @@ void setup() {
   pinMode(SAIL, INPUT);
   
   #ifdef DEBUG
-    //Begin serial communication to computer
+    // Begin serial communication to computer
     Serial.begin(DEBUG_SERIAL_RATE);
   #endif
 
-  //Begin serial communication to RF Module
+  // Begin serial communication to RF Module
   rfSerial.begin(RF_SERIAL_RATE);
 }
 
-//store last rf message sent time
-uint32_t last_send_time;
-
 void loop() {
-  // initialize joystic status variables
-  uint16_t rudr_val = 0;
-  uint16_t sail_val = 0;
+  // initialize mean joystick value variables
+  uint16_t mean_rudr_val = 0;
+  uint16_t mean_sail_val = 0;
+
+  // get joystick status
+  getJoystick(&mean_rudr_val, &mean_sail_val);
+
+  // initialize position variables to 0
   uint8_t rudr_pos = 0;
   int32_t sail_pos = 0;
 
-  //get joystick status
-  getJoystick(&rudr_val, &sail_val);
-
-  //Read returning position data from the boat
+  // Read returning position data from the boat
   getPosition(&rudr_pos, &sail_pos);
 
   #ifdef DEBUG
     // if debugging mode is on, send raw values
-    sendSerialDebug(rudr_pos, sail_pos, rudr_val, sail_val);
+    sendSerialDebug(rudr_pos, sail_pos, mean_rudr_val, mean_sail_val);
   #endif
 
   if(millis() >= last_send_time + SENDING_INTERVAL){
     // Send message via RF when sending interval has expired
-    sendSeralRF(rudr_val, sail_val);
+    sendSeralRF(mean_rudr_val, mean_sail_val);
     last_send_time = millis();
   }
 }
 
 // get joystick status
-void getJoystick(uint16_t* rudr_val, uint16_t* sail_val) {
+void getJoystick(uint16_t* mean_rudr_val, uint16_t* mean_sail_val) {
   // initialize raw value variables and obtain raw values from analog ports
-  *rudr_val = analogRead(RUDR);
-  *sail_val = analogRead(SAIL);
+  rudr_val[val_index] = analogRead(RUDR);
+  sail_val[val_index] = analogRead(SAIL);
+
+  // if index is overflowing, reset to 0
+  if(++val_index == MEAN_NUM) {
+    val_index = 0;
+  }
+  
+  // initialize variable to calculate sum of the array
+  uint32_t rudr_total = 0;
+  uint32_t sail_total = 0;
+
+  // calculate sum for both arrays
+  for(uint8_t index = 0; index < MEAN_NUM; ++index) {
+    rudr_total += rudr_val[index];
+    sail_total += sail_val[index];
+  }
+
+  // Calculate the mean (average) for both arrays
+  *mean_rudr_val = rudr_total / MEAN_NUM;
+  *mean_sail_val = sail_total / MEAN_NUM;
 }
 
 // read RF serial
@@ -88,19 +113,19 @@ void getPosition(uint8_t* rudr_pos, int32_t* sail_pos) {
 }
 
 // send serial message to RF module to instruct what to send
-void sendSeralRF(uint16_t rudr_val, uint16_t sail_val) {
-  rfSerial.print(rudr_val);
+void sendSeralRF(uint16_t mean_rudr_val, uint16_t mean_sail_val) {
+  rfSerial.print(mean_rudr_val);
   rfSerial.print(";");
-  rfSerial.println(sail_val);
+  rfSerial.println(mean_sail_val);
 }
 
 // send serial debug message containing raw values from each analog port
-void sendSerialDebug(uint8_t rudr_pos, int32_t sail_pos, uint16_t rudr_val, uint16_t sail_val) {
+void sendSerialDebug(uint8_t rudr_pos, int32_t sail_pos, uint16_t mean_rudr_val, uint16_t mean_sail_val) {
   Serial.print(rudr_pos);
   Serial.print(";");
   Serial.print(sail_pos);
   Serial.print(";");
-  Serial.print(rudr_val);
+  Serial.print(mean_rudr_val);
   Serial.print(";");
-  Serial.println(sail_val);
+  Serial.println(mean_sail_val);
 }
